45759c0ba13f24973210bb9e835afea4ca22fc85
Edgar24k <edgarlozoya24k@gmail.com>
2021-03-12 13:19:47 -0600

    confirmar

---
+++ imagenes/precache-manifest.c544df672f96ae6ef144c301f79cad08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8654423fc95eac2e21984073b1b66549",
+    "url": "/imagenes/index.html"
+  },
+  {
+    "revision": "bfc4c59df4bb7bca44f5",
+    "url": "/imagenes/static/css/main.c6c3791b.chunk.css"
+  },
+  {
+    "revision": "92adfd0cfc0e924807e2",
+    "url": "/imagenes/static/js/2.f38ce821.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/imagenes/static/js/2.f38ce821.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfc4c59df4bb7bca44f5",
+    "url": "/imagenes/static/js/main.868e4c64.chunk.js"
+  },
+  {
+    "revision": "b42c6a2654e85ae3f04d",
+    "url": "/imagenes/static/js/runtime-main.8c47b523.js"
+  }
+]);
